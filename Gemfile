0ed054e3093c3a9f27856085c434385949404bac
Nathan Schuett <nschuett@gmail.com>
2013-04-22 12:51:21 -0700

    Get video uploads working

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'simple_form'
 gem 'paperclip', '~> 3.0' 
 gem 'aws-sdk', '1.9.1'
 gem 'faker'
-gem 'will_paginate', '3.0.4'
+gem 'will_paginate', "3.0.4"
 gem 'bootstrap-will_paginate'
 
 group :production do
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,11 +4,11 @@ class Pin < ActiveRecord::Base
 	validates :description, presence: true
 	validates :user_id, presence: true
 	validates_attachment :image, presence: true, 
-												content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
-												size: { less_than: 5.megabytes }
+												content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'video/quicktime', 'video/mpeg', 'video/mp4']},
+												size: { less_than: 15.megabytes }
 
 	belongs_to :user
-  has_attached_file :image, styles: { medium: "320x240>" }
+  has_attached_file :image#, styles: { medium: "320x240>" }
 
 
   def image_remote_url=(url_value)
